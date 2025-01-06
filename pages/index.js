3c94ec095556172dc0a5e8c073fe4f8a7455fa17
sariyaesc <escamillasara49@gmail.com>
2025-01-06 17:57:07 -0600

    FEAT: ADD background

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+export default function Home() {
+  return (
+      <>
+        
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="/styles/global.css" />
+        
+        <div className="gradient-bg">
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <filter id="goo">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+                <feBlend in="SourceGraphic" in2="goo" />
+              </filter>
+            </defs>
+          </svg>
+        <div className="gradients-container">
+                <div className="g1"></div>
+                <div className="g2"></div>
+                <div className="g3"></div>
+                <div className="g4"></div>
+                <div className="g5"></div>
+        </div>
+        <div className="interactive"></div>
+        </div>    
+      </>
+  );
+  
+}
