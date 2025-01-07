5148809de5a52968ab6cdddb88977cbb8d95f681
sariyaesc <escamillasara49@gmail.com>
2025-01-06 22:43:26 -0600

    feat: add button and text

---
+++ components/common/Button.js
@@ -0,0 +1,10 @@
+export default function Button({ color, children }) {
+    return (
+      <button
+        className={`rounded-full bg-${color}-500 py-3 px-9 m-9 drop-shadow-md hover:bg-${color}-800 hover:opacity-85 transition duration-300`}
+      >
+        <span className="text-md text-white semibold">{children}</span>
+      </button>
+    );
+  }
+  
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
+import Button from "@/components/common/Button";
 
 export default function Home() {
   return (
       <>
-        
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100..900&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="/styles/global.css" />
         
 
-        <h1 className='z-50 w-screen h-screen flex absolute text-white '>Hola</h1>
+        <div className="relative h-screen flex flex-col items-center justify-center">
+  <h1 className="text-white text-8xl font-bold m-5">Sinestiphy</h1>
+  <p className="text-white text-md">
+    Create playlists based on vibes, visuals, and feelings. 
+  </p>
+  <Button color="green">Log In with Spotify</Button>
+</div>
+
+
         <div className="gradient-bg">
           <svg xmlns="http://www.w3.org/2000/svg">
             <defs>
@@ -20,13 +28,13 @@ export default function Home() {
               </filter>
             </defs>
           </svg>
-        <div className="gradients-container">
+          <div className="gradients-container">
                 <div className="g1"></div>
                 <div className="g2"></div>
                 <div className="g3"></div>
                 <div className="g4"></div>
                 <div className="g5"></div>
-        </div>
+          </div>
         <div className="interactive"></div>
         </div>    
       </>
